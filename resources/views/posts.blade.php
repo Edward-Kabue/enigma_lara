5487b0dae4b89fd3b1e0ff25945f07f09ae5e2b7
Baricho <ekabue0@gmail.com>
2022-06-07 16:23:29 +0300

    Create post page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1>
+        My First Post
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam consequuntur deserunt, dolore doloribus eaque expedita id ipsam iusto, molestiae natus non, obcaecati omnis perferendis placeat quidem velit vero voluptatum.
+    </p>
+</article>
+<article>
+    <h1>
+        My secondPost
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam consequuntur deserunt, dolore doloribus eaque expedita id ipsam iusto, molestiae natus non, obcaecati omnis perferendis placeat quidem velit vero voluptatum.
+    </p>
+</article>
+    </body>
+</html>
